351e2d04f1b0749a1db325de940dcd8b6a66fb36
zhongbaihang <zhongbaihang@nobook.com>
2020-07-29 15:56:00 +0800

    初始化

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve:{
+    extensions:['.js','.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      titel: 'aetherIM',
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
+};
